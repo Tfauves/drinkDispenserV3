bd239115e5db2cfde974807dcfe91fc92f16db0c
Tfauves <tfauvelle@gmail.com>
2021-07-02 21:02:21 -0400

    added syrup and cupHolder classes.

--- src/com/company/CupHolder.java
+++ src/com/company/CupHolder.java
@@ -1,7 +1,24 @@
 package com.company;
 
 public class CupHolder {
+    public int vol;
+    public int qty = 0;
 
+    public CupHolder(int vol) {
+        this.vol = vol;
+    }
+
+    public int dispenseCup() {
+        //decrement qty
+        qty--;
+
+        //return vol
+        return vol;
+    }
+
+    public void refill(int amt) {
+        qty += amt;
+    }
 
 
 }
